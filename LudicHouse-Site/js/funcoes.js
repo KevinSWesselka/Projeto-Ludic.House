65e7fb42ba3e66c62bbb2ed867c7f7d1eb1c9971
KevinSWesselka <kevin.bandtec@gmail.com>
2021-11-30 13:07:07 -0300

    Ajustes BD/Configutações API

--- LudicHouse-Site/js/funcoes.js
+++ LudicHouse-Site/js/funcoes.js
@@ -20,29 +20,6 @@ function validarSessao() {
     }
 }
 
-function limparSessao() {
-    // aguardar();
-    sessionStorage.clear();
-    // finalizarAguardar();
-    window.location = "../login.html";
-}
-
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
 
 // modal
 function mostrarModal() {
--- LudicHouse-Site/src/controllers/usuarioController.js
+++ LudicHouse-Site/src/controllers/usuarioController.js
@@ -64,6 +64,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var estilo = req.body.estiloServer;
 
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
@@ -73,7 +74,7 @@ function cadastrar(req, res) {
         res.status(400).send("Sua senha está undefined!");
     } else {
         
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, estilo)
             .then(
                 function (resultado) {
                     res.json(resultado);
